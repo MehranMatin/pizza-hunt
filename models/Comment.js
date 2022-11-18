2f03aeeb92bd97d701e0fbb8b74e986b1815290c
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-11-17 20:04:55 -0500

    import Types object
    Schema for replies
    
    added virtuals

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,65 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment's _id field
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      type: String
+    },
+    writtenBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // getter
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    toJSON: {
+      getters: true
+    }
   }
+);
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String
+    },
+    commentBody: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema (array of data that adheres to the ReplySchema definition) to validate data for a reply (nested directly in a comment's document, not referred to)
+    replies: [ReplySchema]
+  },
+  {
+    // tell the schema that it can use virtuals
+    toJSON: {
+      // virtuals help extend models by creating a "virtual" field that can be evaluated when the documents are retrieved from the database
+      virtuals: true,
+      // tell the Mongoose model that it should use any getter function we've specified
+      getters: true
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+  }
+);
+
+// virtual for getting total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
