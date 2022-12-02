b0392af213fc664dd3c8eb1e0d341e032631b1a8
Mehran Matin <81208724+MehranMatin@users.noreply.github.com>
2022-12-02 15:33:53 -0500

    removed comments

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -19,7 +19,6 @@ request.onsuccess = function(event) {
 
   // check if app is online, if yes run uploadPizza() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
     uploadPizza();
   }
 };
@@ -83,8 +82,6 @@ function uploadPizza() {
         });
     }
   };
-
-  // more to come...
 }
 
 // listen for app coming back online
